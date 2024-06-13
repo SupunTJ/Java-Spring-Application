1fcf60fa122b6b1a9d06216f098fc3ef187e578b
SupunTJ <jayaweeraspn@gmail.com>
2024-06-13 16:48:43 +0530

    implementing CRUD service

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,9 +1,11 @@
 package com.example.demo.controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.demo.dto.UserDTO;
+import com.example.demo.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 
 @RestController
@@ -11,8 +13,25 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "api/v1/")
 
 public class UserController {
-    @GetMapping("/getUser")
-    public String getUser(){
-        return "All user";
+    @Autowired
+    private UserService userService;
+    @GetMapping("/getusers")
+    public List<UserDTO> getUsers(){
+        return userService.getAllUsers();
+    }
+
+    @PostMapping("/adduser")
+    public UserDTO saveUser(@RequestBody UserDTO userDTO){
+        return  userService.saveUser(userDTO);
+    }
+
+    @PutMapping("/updateuser")
+    public UserDTO updateUser(@RequestBody UserDTO userDTO){
+        return userService.updateUser(userDTO);
+    }
+
+    @DeleteMapping("/deleteuser")
+    public String deleteUser(@RequestBody UserDTO userDTO){
+        return userService.deleteUser(userDTO);
     }
 }
